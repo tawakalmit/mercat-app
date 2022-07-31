b55d112751ccc487218516bce0610cb167b64ae5
tawakalmit <tawakal.mit@gmail.com>
2022-08-01 03:33:36 +0700

    add logo to login page mobile view

--- pages/login.js
+++ pages/login.js
@@ -9,13 +9,12 @@ export default function login() {
   return (
     <div className="flex align-items:center w-full min-h-screen bg-cover bg-[url('../asset/mercat.jpg')]">
                 <div className="w-8/12 m-auto flex justify-between items-center md:h-fit">
-                    <div className="hidden w-1/2 md:block 2xl:scale-150">
+                    <div className="hidden w-1/2 md:block">
                         <Image src={Logo} className="w-80" />
-                        <h2 className="text-3xl font-semibold text-white text-center">One stop online shop</h2>
+                        <h2 className="invisible text-3xl font-semibold text-white text-center md:visible">One stop online shop</h2>
                     </div>
                     <div className="p-6 rounded-2xl bg-transparent h-fit w-80 flex flex-col mt-10 md:bg-white 2xl:scale-150">
-                        <h1 className='text-white font-semibold text-2xl mx-auto md:text-black'>Login</h1>
-                        <h2 className='text-center text-white text-md opacity-70 mx-auto mb-6 md:text-black'></h2>
+                        <div className='md:hidden'><Image src={Logo} /></div>
                         <form>
                             <CustomInput 
                             ptext="User Name"
