a826c7415949c422d8f1e3b157b1dd0816b311ee
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-01 17:17:47 +0700

    Shopping Cart Page Styling was cleared

---
+++ pages/user/productpages/ShoppingCart.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import DashboardCard from '../components/DashboardCard';
+
+const ShoppingCart =() => {
+    return (
+        <>
+        <NavBar />
+        <div className="w-full h-auto flex justify-center my-5">
+            <h2 className="font-semibold text-lg text-center">Shopping Cart</h2>
+        </div>
+        <div className="grid justify-items-center sm:grid-cols-2 sm:px-0 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-5 w-screen h-auto p-5">
+        <DashboardCard />
+        <DashboardCard />
+        <DashboardCard />
+        <DashboardCard />
+        <DashboardCard />
+        <DashboardCard />
+        <DashboardCard />
+        </div>
+        </>
+    )
+}
+export default ShoppingCart;
