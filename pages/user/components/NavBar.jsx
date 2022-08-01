7f29272a982a6d3420ad8a2e86aa9e9fa307ff22
tawakalmit <tawakal.mit@gmail.com>
2022-08-01 14:15:12 +0700

    change navbar color

--- pages/user/components/NavBar.jsx
+++ pages/user/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { CgProfile } from "react-icons/cg";
 
 const NavBar =()=>{
     return(
-        <div className="w-full h-none bg-lime-400 flex justify-around p-5">
+        <div className="w-full h-none bg-[#94CD32] flex justify-around p-5">
         <IoStorefront className="h-11 w-auto flex justify-center text-white"/>
         <HiMoon className="h-11 w-auto text-white"/>
         <HiOutlineShoppingCart className="h-11 w-auto text-white"/>
