5b8c9452b2a2f188c3dfc8ecb9c62171edc78c97
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-01 18:30:00 +0700

    function subtotal sedang dalam proses perbaikan

--- pages/user/productpages/ShoppingCart.jsx
+++ pages/user/productpages/ShoppingCart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import DashboardCard from '../components/DashboardCard';
+import ShoppingCartCard from '../components/ShoppingCartCard';
 
 const ShoppingCart =() => {
     return (
@@ -10,13 +10,7 @@ const ShoppingCart =() => {
             <h2 className="font-semibold text-lg text-center">Shopping Cart</h2>
         </div>
         <div className="grid justify-items-center sm:grid-cols-2 sm:px-0 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-5 w-screen h-auto p-5">
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
+        <ShoppingCartCard />
         </div>
         </>
     )
