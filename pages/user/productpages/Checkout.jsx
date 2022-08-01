3e4375913d26daa7e026224ab216c2bac8147682
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-02 01:53:38 +0700

    checkout page styling was cleared

---
+++ pages/user/productpages/Checkout.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+
+const Checkout =()=>{
+    return(
+        <>
+        <div className="w-full h-none bg-lime-400 flex justify-between py-5 px-7">
+            <div className="flex justify-start">
+            <IoIosArrowBack className="h-11 w-auto flex justify-center text-white"/>
+            <h2 className="text-white text-2xl font-semibold mt-1 ml-4">Products</h2>
+            </div>
+        </div>
+        <div className="w-full h-auto pt-7 px-8 flex justify-between">
+            <h2 className="font-bold text-2xl">Total</h2>
+            <h2 className="text-xl">IDR 20.000</h2>
+        </div>
+        <div className="w-full h-auto pt-7 px-8 flex justify-between">
+            <h2 className="text-sm">Payment Method</h2>
+            <h2 className="text-sm"></h2>
+        </div>
+        <div className="w-full h-auto pt-7 px-8 flex justify-between">
+            <h2 className="text-sm">Address</h2>
+            <h2 className="text-lime-400 text-sm w-40 h-auto sm:w-56 md:w-auto lg:w-auto text-right">TB. Simatupang, Jl. Nangka Raya No.58 C, RW.5, Tj. Baru, Kec. Jagakarsa, Kota Jaksel, DKI Jakarta 12530</h2>
+        </div>
+        <div className="w-full h-auto mt-9 flex justify-center my-2 ">
+        <button className="bg-slate-400 rounded-3xl w-36 h-10 font-bold text-lg text-white">Checkout</button>
+        </div>
+        </>
+    )
+}
+export default Checkout;
