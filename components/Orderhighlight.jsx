b2fd364ea6feb9b957b17f66198aec9624143643
tawakalmit <tawakal.mit@gmail.com>
2022-08-01 13:37:52 +0700

    add user profile page

---
+++ components/Orderhighlight.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Orderhighlight() {
+
+    const handleCancel = (e) => {
+        alert("Are you sure ?")
+    }
+
+  return (
+    <div className='flex items-center justify-around mb-5 w-10/12 mx-auto'>
+        <Link href={"/detailhistoryorder"}><div className='flex w-10/12 justify-between bg-[#9ACD32] p-2 rounded-xl text-white cursor-pointer'>
+            <p>Men's Shirt</p>
+            <p>5</p>
+            <p>IDR 200.000</p>
+        </div></Link>
+        <span onClick={(e) => handleCancel(e)} className='mr-5 text-2xl text-[#e74c3c] cursor-pointer'>X</span>
+    </div>
+  )
+}
---
+++ components/Userhighlight.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { CgProfile } from "react-icons/cg";
+import {FiLogOut} from "react-icons/fi"
+import Link from 'next/link';
+
+import Button from './Button';
+
+export default function Userhighlight() {
+  return (
+    <div className='flex w-3/6 mx-auto mt-5 items-center justify-center'>
+        <CgProfile color='#2c3e50' size={60} />
+        <div className='flex flex-col justify-center ml-2'>
+            <h1 className='text-3xl'>Mugiwara</h1>
+            <div className='flex items-center mt-1'>
+                <Link href={"/editprofile"}><Button kelas={"bg-[#65BDBE] w-28 h-6 rounded-full text-[#ecf0f1] cursor-pointer"} label="edit profile" /></Link>
+                <Link href={"/login"}><FiLogOut className='ml-3 cursor-pointer' /></Link>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
