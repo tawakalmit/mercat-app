eb96df6cfc5b361c6359adba2c71affbf9eed620
ikram abdillah <ikram.koli@gmail.com>
2022-08-02 14:49:13 +0700

    testapi

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ function Layout(props) {
   return (
     <div className="w-full h-screen flex flex-col overflow-auto">
       <NavBack />
-      <div className="h-full w-full bg-white dark:bg-black bg-center bg-cover bg-no-repeat flex flex-col">{props.children}</div>
+      <div>{props.children}</div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Checkout from "./user/productpages/Checkout";
+import login from "./login";
 
 export default function Home() {
   return (
     <>
-      <Checkout />
+      <login />
     </>
   );
 }
