b1fd97fac7e2dadcc9cd90098bddd86fea7edf02
tawakalmit <tawakal.mit@gmail.com>
2022-08-03 09:55:36 +0700

    fetch data products page

--- components/dashboardcard.jsx
+++ components/dashboardcard.jsx
@@ -3,49 +3,43 @@ import Image from 'next/image';
 import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 import Link from 'next/link';
 
-const DashboardCard = () =>{
-    const [counter, setCounter]=useState(0);
-    const [stock, setStock]=useState(10);
+const DashboardCard = ({productname, price, stok}) =>{
 
+    const [counter, setCounter]=useState(0);
+    
     const increment =()=>{
-        if(stock>=1){
         setCounter(counter+1);
-        setStock(stock-1);
-        }
     }
     
     const decrement =()=>{
-        if(counter>=1){
-            setCounter(counter-1);
-            setStock(stock+1);
-        }
+        setCounter(counter-1);
     }
 
     return(
         <div className="mb-5">
-            <div className="card h-auto w-36 border-solid border-2 border-lime-400 rounded-3xl p-2">
-                <Image src="/tashitam.jpg" width="129" height="112" className='rounded-t-2xl'/>
-                <h2 className="text-sm font-bold mt-1.5">Men's Bag</h2>
-                <h3 className="text-[0.6rem] font-medium text-lime-400 mt-1.5">IDR 20.000</h3>
+            <div className="h-fit text-center m-5 w-36 flex-col border-solid border-2 border-[#94CD32] rounded-2xl p-2 md:w-56">
+                <Image src="/tashitam.jpg" width="300" height="300" className='rounded-t-2xl'/>
+                <h2 className="text-2xl">{productname}</h2>
+                <h3 className="text-[#9ACD32] text-m">IDR {price}</h3>
                 <div className="mt-0.5 w-full h-auto flex justify-between gap-0">
-                <p className="text-xs font-medium">Stock</p>
-                <p className="text-xs font-medium">{stock}</p>
+                    <p className="text-xs font-medium">Stock</p>
+                    <p className="text-xs font-medium">{stok}</p>
                 </div>
                 <div className="mt-0.5 w-full h-auto flex justify-between">
-                <p className="text-xs font-medium">Quantity</p>
-                <div className="gap-x-0 flex justify-between border-lime-400 border-solid border-y-2 text-center">
-                    <button className='w-5 h-4.5 border-lime-400 border-solid border-x-2' onClick={increment}>
-                    <IoIosArrowUp className='w-4'/>
-                    </button>
-                    <div className="w-6 h-4.5 text-center text-slate-400">{counter}</div>
-                    <button className='w-5 h-4.5 border-lime-400 border-solid border-x-2'>
-                    <IoIosArrowDown className='w-4' onClick={decrement}/>
-                    </button>
+                    <p className="text-xs font-medium">Quantity</p>
+                    <div className="gap-x-0 flex justify-between border-lime-400 border-solid border-y-2 text-center">
+                        <button className='w-5 h-4.5 border-lime-400 border-solid border-x-2' onClick={increment}>
+                        <IoIosArrowUp className='w-4'/>
+                        </button>
+                        <div className="w-6 h-4.5 text-center text-slate-400">{counter}</div>
+                        <button className='w-5 h-4.5 border-lime-400 border-solid border-x-2'>
+                            <IoIosArrowDown className='w-4' onClick={decrement}/>
+                        </button>
                     </div>
                 </div>
                 <div className="mt-1.5 flex items-center justify-around">
-                <Link href='/user/shoppingcart'><button className="rounded-full w-12 h-5.5 bg-slate-400 text-xs text-white">Buy</button></Link>
-                <button className="rounded-full w-12 h-5.5 bg-white text-xs text-slate-400 border-2 border-slate-400">+Cart</button>
+                <Link href='/user/shoppingcart'><button className="rounded-full w-12 h-5.5 bg-slate-400 text-xs text-white md:w-20 h-7">Buy</button></Link>
+                <button className="rounded-full w-12 h-5.5 bg-white text-xs text-slate-400 border-2 border-slate-400 md:w-20 h-7">+Cart</button>
                 </div>
             </div>
         </div>
--- pages/user/productpage.jsx
+++ pages/user/productpage.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import DashboardCard from '../../components/dashboardcard';
 import NavBar from '../../components/navbar';
 
 const ProductPage =() => {
+
+    const [datas, setDatas] = useState([])
+  const [productname, setproductname] = useState("")
+  const [price, setprice] = useState("")
+  const [stock, setstock] = useState("")
+  const [loading, setLoading] = useState(false)
+  
+
+  useEffect(() => {
+    fetchData();
+  }, [productname, price, stock]);
+
+  const fetchData = () => {
+    var requestOptions = {
+      method: "get",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch(
+      "https://virtserver.swaggerhub.com/DianNurdiana-alt/E-STORE/1.0.0/products",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        const { data } = result
+        setDatas(data)
+      })
+      .catch((err) => {
+        alert(err.toString())
+      })
+      .finally(() => setLoading(false));
+  }
+
+    
+
     return(
         <>
         <NavBar />
-        <div className="grid justify-items-center sm:grid-cols-2 sm:px-0 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-5 w-screen h-auto p-5">
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
+        <div className="w-full flex-wrap mx-auto mt-5 flex justify-around md:w-10/12">
+        {datas.map((data) => (
+          <DashboardCard
+          key={data.productname}
+          productname={data.productname}
+          price={data.price.toLocaleString()}
+          stok={data.stock}
+          />
+        ))}
+        
         </div>
         </>
     )
