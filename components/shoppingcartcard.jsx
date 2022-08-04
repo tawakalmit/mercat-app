32f4b4ffce706e3e160a896ed11a0dee0b36f4bf
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-04 22:22:56 +0700

    delete and display shoppingcart

--- components/shoppingcartcard.jsx
+++ components/shoppingcartcard.jsx
@@ -1,35 +1,65 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Image from 'next/image';
 import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
-const DashboardCard = () =>{
-    const [counter, setCounter]=useState(0);
-    const [stock, setStock]=useState(10);
-    const [harga]=useState(20000)
-    const [subtotal,setSubtotal]=useState(0);
+import { useRouter } from "next/router";
 
+const DashboardCard = ({productname, stock, price, quantity, subtotal}) =>{
+    const [counter, setCounter]=useState(0);
+    const [loading, setLoading]=useState(true);
+    const route = useRouter();
+
+    const handleDelete = async (e) => {
+        setLoading(true);
+        e.preventDefault();
         
-        const increment =()=>{
-            if(stock>=1){
-                setCounter(counter+1);
-                // setStock(stock-1);
-                setSubtotal((counter+1)*harga);
+        var requestOptions = {
+          method: "DELETE",
+          headers: { "Content-Type": "aplication/json" },
+        };
+        fetch(`https://virtserver.swaggerhub.com/DianNurdiana-alt/E-STORE/1.0.0/carts/{productid}`, requestOptions)
+          .then((response) => response.json())
+          .then((result) => {
+            console.log(result);
+            const { message } = result;
+            console.log('ini console', message);
+            if (result.code === 200) {
+              if (message) {
+                route.push("/shoppingcart");
+              }
             }
-        }
+            alert(message);
+          })
+          .catch((error) => {
+            alert(error.toString());
+          })
+          .finally(() => setLoading(false));
+      };
+      
+      
+
+        // const increment =()=>{
+        //     if(stock>=1){
+        //         setCounter(counter+1);
+        //         setStock(stock-1);
+        //         // setSubtotal((counter+1)*harga);
+        //     }
+        // }
         
-        const decrement =()=>{
-            if(counter>=1){
-            setCounter(counter-1);
-            // setStock(stock+1);
-            setSubtotal((counter-1)*harga)}
-        }
+        // const decrement =()=>{
+        //     if(counter>=1){
+        //     setCounter(counter-1);
+        //     // setStock(stock+1);
+        //     // setSubtotal((counter-1)*harga)
+        //   }
+        // }
     
 
     return(
         <div className="mb-5">
             <div className="card h-auto w-36 border-solid border-2 border-lime-400 rounded-3xl p-2">
                 <Image src="/tashitam.jpg" width="129" height="112" className='rounded-t-2xl' alt='pic'/>
-                <h2 className="text-sm font-bold mt-1.5">Mens Bag</h2>
-                <h3 className="text-[0.6rem] font-medium text-lime-400 mt-1.5">IDR 20.000</h3>
+                <h2 className="text-sm font-bold mt-1.5">{productname}</h2>
+                <h3 className="text-[0.6rem] font-medium text-lime-400 mt-1.5">{price}</h3>
                 <div className="mt-0.5 w-full h-auto flex justify-between gap-0">
                 <p className="text-xs font-medium">Stock</p>
                 <p className="text-xs font-medium">{stock}</p>
@@ -37,18 +67,24 @@ const DashboardCard = () =>{
                 <div className="mt-0.5 w-full h-auto flex justify-between">
                 <p className="text-xs font-medium">Quantity</p>
                 <div className="gap-x-0 flex justify-between border-lime-400 border-solid border-y-2 text-center">
-                    <button className='w-5 h-4.5 border-lime-400 border-solid border-x-2' onClick={increment}>
+                    <button className='w-5 h-4.5 border-lime-400 border-solid border-x-2'>
                     <IoIosArrowUp className='w-4'/>
                     </button>
-                    <div className="w-6 h-4.5 text-center text-slate-400">{counter}</div>
+                    <div className="w-6 h-4.5 text-center text-slate-400">{quantity}</div>
                     <button className='w-5 h-4.5 border-lime-400 border-solid border-x-2'>
-                    <IoIosArrowDown className='w-4' onClick={decrement}/>
+                    <IoIosArrowDown className='w-4'/>
                     </button>
                     </div>
                 </div>
                     <div className="mt-0.5 w-full h-auto flex justify-between gap-0">
                     <p className="text-xs font-medium">Subtotal</p>
                     <p className="text-xs font-medium">IDR {subtotal}</p>
+                    </div>
+
+                    <div className='flex justify-center'>
+                    <button className="w-11 h-5 bg-slate-400 text-white text-xs rounded-full md:w-20" onClick={(e) => handleEdit(e)}>Edit</button>
+                    
+                    <button className="w-11 h-5 bg-[#e74c3c] ml-2.5 text-white text-xs rounded-full md:w-20" onClick={(e) => handleDelete(e)}>Delete</button>
                     </div>
             </div>
         </div>
--- pages/shoppingcart.jsx
+++ pages/shoppingcart.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import NavBar from '../components/navbar';
 import ShoppingCartCard from '../components/shoppingcartcard';
 import Link from 'next/link';
 
 const ShoppingCart =() => {
+const [datas, setDatas] = useState([]);
+const [loading, setLoading]=useState(true);
+const [price, setPrice]= useState(0)
+
+
+    useEffect(() => {
+        fetchData();
+      }, []);
+
+      const fetchData = () => {
+        var requestOptions = {
+          method: "get",
+          headers: { "Content-Type": "application/json" },
+        };
+        fetch("https://virtserver.swaggerhub.com/DianNurdiana-alt/E-STORE/1.0.0/carts", requestOptions)
+          .then((response) => response.json())
+          .then((result) => {
+            const { data } = result;
+            setDatas(data)
+          })
+          .catch((err) => {
+            alert(err.toString());
+          })
+          .finally(() => setLoading(false));
+      };
+
     return (
         <>
         <NavBar />
@@ -11,10 +37,9 @@ const ShoppingCart =() => {
             <h2 className="font-semibold text-lg text-center">Shopping Cart</h2>
         </div>
         <div className="grid justify-items-center sm:grid-cols-2 sm:px-0 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-5 w-screen h-auto p-5">
-        <ShoppingCartCard />
-        <ShoppingCartCard />
-        <ShoppingCartCard />
-        <ShoppingCartCard />
+        {datas.map((data) => (
+        <ShoppingCartCard key={data.productname} productname={data.productname} price={data.price.toLocaleString()} stock={data.stock} quantity={data.quantity}subtotal={data.subtotal}/>
+        ))}
         </div>
         <div className="flex justify-center w-full h-auto">
         <Link href='/user/checkout'><button className='text-white bg-slate-400 w-36 h-10 rounded-3xl md:mt-5 lg:mt-5'>Order</button></Link>
