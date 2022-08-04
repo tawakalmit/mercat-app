0c0815de7532767f6e5866f4a061bac67af99b31
tawakalmit <tawakal.mit@gmail.com>
2022-08-04 23:43:27 +0700

    add product form data integration

--- pages/addproduct.jsx
+++ pages/addproduct.jsx
@@ -3,9 +3,11 @@ import { useRouter } from "next/router";
 import { IoChevronBackSharp } from "react-icons/io5";
 import CustomInput from "../components/CustomInput";
 import Button from "../components/Button";
+import Image from "next/image";
 
 function Addproduct() {
   const route = useRouter();
+  const [objSubmit, setObjSubmit] = useState("")
   const [productid, setProdctid] = useState(0);
   const [productpic, setProductpic] = useState("");
   const [productname, setProductname] = useState("");
@@ -14,43 +16,76 @@ function Addproduct() {
   const [loading, setLoading] = useState(false);
   const [disabled, setDisabled] = useState(true);
 
-  useEffect(() => {
-    if (productpic && productname && price && stock) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-  }, [productpic, productname, price, stock]);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // const fetchData = async () => {
+  //   var requestOptions = {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization: `Bearer ${localStorage.getItem("token")}`,
+  //     },
+  //   };
+
+  //   fetch(
+  //     "https://projectbiasa.site/products",
+  //     requestOptions
+  //   )
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       console.log(result)
+  //       // const { message, code, data } = result;
+  //       // const { email, first_name, last_name, image } = data;
+  //       // if ([401, 403].includes(code)) {
+  //       //   localStorage.removeItem("token");
+  //       //   setToken("0");
+  //       //   router.push("/login");
+  //       //   alert(message);
+  //       // } else {
+  //       //   setEmail(email);
+  //       //   setFirstName(first_name);
+  //       //   setLastName(last_name);
+  //       //   const insertHTTPS = image.replace("http", "https");
+  //       //   setImage(insertHTTPS);
+  //       // }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     })
+  //     .finally(() => setLoading(false));
+  // };
 
   const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
-    const body = {
-      productpic,
-      productname,
-      price,
-      stock,
-    };
+    const formData = new FormData();
+    for (const key in objSubmit) {
+      formData.append(key, objSubmit[key]);
+    }
     var requestOptions = {
       method: "POST",
-      headers: { "Content-Type": "aplication/json" },
-      body: JSON.stringify(body),
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: formData,
     };
+
     fetch("https://projectbiasa.site/admin/products", requestOptions)
       .then((response) => response.json())
       .then((result) => {
         const { message } = result;
-        if (result.code === 200) {
-          if (message) {
-            route.push("/dasboard");
-          }
-        }
         alert(message);
+        setObjSubmit({});
       })
-      .catch((error) => {
-        alert(error.toString());
-      })
+      .catch((error) => console.log("error", error))
       .finally(() => setLoading(false));
+  };
+
+  const handleChange = (value, key) => {
+    let temp = { ...objSubmit };
+    temp[key] = value;
+    setObjSubmit(temp);
   };
 
   return (
@@ -63,13 +98,19 @@ function Addproduct() {
 
       <div className="flex flex-row items-center justify-center mt-10">
         <form className="flex flex-col gap-4 min-w-[40%]" onSubmit={(e) => handleSubmit(e)}>
-          <CustomInput ptext="Name Product" id="input-product" type="name product" placeholder="product" onChange={(e) => setProductname(e.target.value)} />
 
-          <CustomInput ptext="Price" id="input-price" type="price" placeholder="price" onChange={(e) => setPrice(e.target.value)} />
+          <Image src={productpic} alt={productpic} width={200} height={200} />
 
-          <CustomInput ptext="Stock" id="input-stock" type="text" placeholder="0" onChange={(e) => setStock(e.target.value)} />
+          <CustomInput ptext="Name Product" id="input-product" type="name product" placeholder="product" onChange={(e) => handleChange(e.target.value, "productname")} />
 
-          <input type='file' id="input-file"/>
+          <CustomInput ptext="Price" id="input-price" type="price" placeholder="price" onChange={(e) => handleChange(e.target.value, "price")} />
+
+          <CustomInput ptext="Stock" id="input-stock" type="text" placeholder="0" onChange={(e) => handleChange(e.target.value, "stock")} />
+
+          <input type='file' id="input-file" onChange={(e) => {
+                setProductpic(URL.createObjectURL(e.target.files[0]));
+                handleChange(e.target.files[0], "productpic");
+              }}/>
           <div className="flex flex-row items-center justify-center pt-10">
             <Button kelas={"bg-[#9ACD32] text-white font-bold py-2 px-4 rounded-full w-50 mt-2 md: rounded w-35"} label="Add Product" />
           </div>
