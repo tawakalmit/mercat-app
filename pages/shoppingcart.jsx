dc9df20ac948d686ffc4a63770425d795f03285e
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-05 10:24:10 +0700

    dapatkan token shopping cart

--- pages/shoppingcart.jsx
+++ pages/shoppingcart.jsx
@@ -20,13 +20,16 @@ const ShoppingCart = () => {
   const fetchData = () => {
     var requestOptions = {
       method: "get",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MSwiUm9sZSI6ImFkbWluIn0.wL5LPPl44CKPjG4og_zU9lPw9_MlObWa2EWebWh2lFY`,
+      "Content-Type": "application/json" },
     };
-    fetch("https://virtserver.swaggerhub.com/DianNurdiana-alt/E-STORE/1.0.0/carts", requestOptions)
+    fetch("https://projectbiasa.site/carts", requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        const { data } = result;
-        setDatas(data);
+        const { code, data, message, userid } = result;
+        console.log('ini data',data)
+        setDatas(data)
+        console.log('ini datas',datas)
       })
       .catch((err) => {
         alert(err.toString());
@@ -49,7 +52,7 @@ const ShoppingCart = () => {
       header: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
     };
-    fetch("https://virtserver.swaggerhub.com/DianNurdiana-alt/E-STORE/1.0.0/orders", requestOptions)
+    fetch("https://projectbiasa.site/orders", requestOptions)
       .then((response) => response.json())
       .then((result) => {
         const { message } = result;
@@ -61,23 +64,23 @@ const ShoppingCart = () => {
       })
       .finally(() => setLoading(false));
   };
-  return (
-    <>
-      <NavBar />
-      <div className="w-full h-auto flex justify-center my-5">
-        <h2 className="font-semibold text-lg text-center">Shopping Cart</h2>
-      </div>
-      <div className="grid justify-items-center sm:grid-cols-2 sm:px-0 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-5 w-screen h-auto p-5">
-        {datas.map((data) => (
-          <ShoppingCartCard key={data.productname} productname={data.productname} price={data.price.toLocaleString()} stock={data.stock} quantity={data.quantity} subtotal={data.subtotal} />
-        ))}
-      </div>
-      <div className="flex justify-center w-full h-auto">
-        <button onClick={handleOrder} className="text-white bg-slate-400 w-36 h-10 rounded-3xl md:mt-5 lg:mt-5">
-          Order
-        </button>
-      </div>
-    </>
-  );
+  //return (
+    // <>
+    //   <NavBar />
+    //   <div className="w-full h-auto flex justify-center my-5">
+    //     <h2 className="font-semibold text-lg text-center">Shopping Cart</h2>
+    //   </div>
+    //   <div className="grid justify-items-center sm:grid-cols-2 sm:px-0 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-5 w-screen h-auto p-5">
+    //     {datas.map((data) => (
+    //       <ShoppingCartCard key={data.productname} productname={data.productname} price={data.price.toLocaleString()} stock={data.stock} quantity={data.quantity} subtotal={data.subtotal} />
+    //     ))}
+    //   </div>
+    //   <div className="flex justify-center w-full h-auto">
+    //     <button onClick={handleOrder} className="text-white bg-slate-400 w-36 h-10 rounded-3xl md:mt-5 lg:mt-5">
+    //       Order
+    //     </button>
+    //   </div>
+    // </>
+  //);
 };
 export default ShoppingCart;
